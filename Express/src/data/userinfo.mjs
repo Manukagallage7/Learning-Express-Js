0963d2b6ce4e906621faf2d2c70a1c72ab8e4c92
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-15 00:38:16 +0530

    feat: add userInfo data module with sample user entries

---
+++ Express/src/data/userinfo.mjs
@@ -0,0 +1,34 @@
+export const userInfo = [
+    {
+        id: 1, name: 'John Doe', email: 'john@gmail.com'
+    },
+    {
+        id: 2, name: 'Jane Doe', email: 'jane@gmail.com',
+    },
+    {
+        id: 3, name: 'Smith Doe', email: 'smith@gmail.com',
+    },
+    {
+        id: 4, name: 'Will Doe', email: 'will@gmail.com',
+    },
+    {
+        id: 5, name: 'Kathy Doe', email: 'kathy@gmail.com',
+    },
+    {
+        id: 6, name: 'Sam Doe', email: 'sam@gmail.com',
+    },
+    {
+        id: 7, name: 'Sara Doe', email: 'sara@gmail.com',
+    },
+    {
+        id: 8, name: 'Tom Doe', email: 'tom@gmail.com',
+    },
+    {
+        id: 9, name: 'Jerry Doe', email: 'jerry@gmail.com',
+    },
+    {
+        id: 10, name: 'Mickey Doe', email: 'mickey@gmail.com',
+
+    }
+]
+
