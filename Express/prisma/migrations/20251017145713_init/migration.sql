a67655647c64da0f4369d28f3b3db7e1bf5d4f8a
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-17 20:28:49 +0530

    feat: add Prisma setup with User model and initial migration
    - Added Prisma client and dev dependencies to package.json
    - Created initial migration for User table with unique indexes on Username and Email
    - Added migration lock file for PostgreSQL provider
    - Defined Prisma schema for User model with appropriate fields and constraints

---
+++ Express/prisma/migrations/20251017145713_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "Id" SERIAL NOT NULL,
+    "Name" TEXT NOT NULL,
+    "Username" TEXT NOT NULL,
+    "Email" TEXT NOT NULL,
+    "Password" TEXT NOT NULL,
+    "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "UpdatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Username_key" ON "User"("Username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_Email_key" ON "User"("Email");
