1a435bca0124692e2b5ed5301e7352852a97136c
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-22 19:45:06 +0530

    enhance userRouter with async handlers and improve response messages

--- Express/src/router/user.mjs
+++ Express/src/router/user.mjs
@@ -5,15 +5,26 @@ import DB from "../db/db.mjs"
 const userRouter = Router()
 
 //Get all users
-userRouter.get('/allUsers',(req, res)=>{
-    res.status(200).json({
-        msg: 'get all users',
-        data: userInfo,
-    })
+userRouter.get('/allUsers', async(req, res)=>{
+
+    try{
+        const userData = await DB.user.findMany()
+        return res.status(200).json({
+            msg: 'Get All Users',
+            data : userData,
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            msg: "Failed to fetch users",
+            data: null,
+        })
+    }
+    
 })
 
 //Get user by ID
-userRouter.get('/byUserId',(req, res)=>{
+userRouter.get('/byUserId', async (req, res)=>{
     const {id} = req.query
     if(!id){
         return res.status(400).json({
@@ -21,11 +32,21 @@ userRouter.get('/byUserId',(req, res)=>{
             data: null,
         })
     }
-    const user = userInfo.find(user => user.id === parseInt(id))
-    return res.status(200).json({
-        msg: 'get user by ID',
-        data: user,
-    })
+    try{
+        const userData = await DB.user.findUnique({
+            where: {Id: Number(id)}
+        })
+        return res.status(200).json({
+            msg: 'Get User by ID',
+            data : userData,
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Failed the fetch user by ID',
+            data: null,
+        })
+    }
 
 })
 
@@ -38,7 +59,10 @@ userRouter.post('/create-user', async (req, res)=>{
     
     try {
         const newUser = await DB.user.create({data: userData})
-        return res.status(201).json({newUser})
+        return res.status(201).json({
+            msg: 'User Created Successfully',
+            data: newUser,
+        })
     }catch(error){
         console.log(error)
         return res.status(500).json({
@@ -47,8 +71,60 @@ userRouter.post('/create-user', async (req, res)=>{
     }
 })
 
+//Update User
 
+userRouter.put('/update-user', async (req, res)=>{
+    const {id} = req.body
+    if(!id){
+        return res.status(400).json({
+            msg: 'User ID is required',
+            data: null,
+        })
+    }
+    try{
+        const updatedUser = await DB.user.update({
+            where: {Id: Number(id)},
+        })
+        return res.status(200).json({
+            msg: 'User Updated Successfully',
+            data: updatedUser,
+        })
 
+    }catch(error){
+        console.log(error)
+        return res.status(500).js0n({
+            msg: 'User Update failed',
+            data: null,
+        })
+    }
+})
+
+//Delete User 
+
+userRouter.delete('/delete-user', async (req, res)=>{
+    const {id} = req.body
+    if(!id){
+        return res.status(400).json({
+            msg: 'User ID is required',
+            data: null,
+        })
+    }
+    try{
+        const deletedUser = await DB.user.delete({
+            where: {Id: Number(id)},
+        })
+        return res.status(200).json({
+            msg: 'User Deleted Successfully',
+            data: deletedUser,
+        })
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            msg: 'User Delete failed',
+            data: null,
+        })
+    }
+})
 
 //Login
 
