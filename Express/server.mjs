d7a3c6d50f8e357dde5f79d1c888be469cb90f77
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-23 23:22:21 +0530

    add testRouter to server configuration

--- Express/server.mjs
+++ Express/server.mjs
@@ -1,12 +1,14 @@
 import express from 'express';
 import productRouter from './src/router/product.mjs';
 import userRouter from './src/router/user.mjs';
+import testRouter from './src/router/test.mjs';
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/v1/user', userRouter);
 server.use('/api/v1/product', productRouter);
+server.use('/api/v1/test', testRouter);
 
 server.listen(5000, ()=> {
     console.log('Server is running on http://localhost:5000');
