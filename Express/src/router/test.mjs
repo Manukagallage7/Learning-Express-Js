3a5ce7c01019533186a1d95c8c8e139282b90c32
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-26 22:17:02 +0530

    streamline GET endpoint structure and integrate middleware

--- Express/src/router/test.mjs
+++ Express/src/router/test.mjs
@@ -1,19 +1,18 @@
 import {Router} from 'express'
+import testMiddleware from '../utils/testMiddleware.mjs'
 
 const testRouter = Router()
 
-testRouter.get('/', (req, res, next)=> {
-    console.log('1')
-    next()
-},
-(req, res, next) => {
-    console.log('2')
-    next()
-},
-(req, res) => {
-    console.log('3')
-    res.sendStatus(200)
-}
+testRouter.get('/',
+    testMiddleware,
+    (req, res, next) => {
+        console.log('2')
+        next()
+    },
+    (req, res) => {
+        console.log('3')
+        res.sendStatus(200)
+    }
 )
 
 export default testRouter
