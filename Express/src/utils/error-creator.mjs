64746cc55da4950517ba223a1e6f367e76e19300
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-27 21:57:21 +0530

    add loginError utility function to format error messages

---
+++ Express/src/utils/error-creator.mjs
@@ -0,0 +1,14 @@
+export const loginError = (error)=> {
+    const errorMsg = {}
+
+    for(const e of error){
+        if(errorMsg[e.path]!== undefined) {
+            const msg = '${errorMsg[e.path]} , ${e.msg}'
+            errorMsg[e.path] = msg
+        } else {
+            errorMsg[e.path] = e.msg
+        }
+    }
+
+    return errorMsg
+}
