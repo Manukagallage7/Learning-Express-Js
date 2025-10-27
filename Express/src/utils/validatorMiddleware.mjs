cf1d6ca5d8e956a3ae07485d1f69251d6a4629e1
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-27 21:57:32 +0530

    add validation functions for user registration and login

--- Express/src/utils/validatorMiddleware.mjs
+++ Express/src/utils/validatorMiddleware.mjs
@@ -9,3 +9,19 @@ export const validatorMethod = ()=> [
         .withMessage('not a number'),
     query('city').isString().withMessage('not a string'),
 ]
+
+export const registerValidate= ()=> [
+    body('Username').notEmpty().withMessage('Please Enter the Username'),
+    body('Password').notEmpty().withMessage('Password not found').isStrongPassword({minlength: 8, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1}).withMessage('Password is not strong enough'),
+    body('Name').notEmpty().withMessage('Please Enter the Name'),
+    body('Email').isEmail().withMessage('Please Enter a valid Email'),
+]
+
+export const comValidate = (...keys)=> {
+    const loginVa = []
+    keys.forEach(k=>{
+        loginVa.push(body(k).notEmpty().withMessage(`Please Enter the ${k}`))
+    })
+
+    return loginVa;
+}
