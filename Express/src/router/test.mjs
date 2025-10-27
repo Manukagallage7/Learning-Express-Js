8066b328ec2077adbeaf7f127e266871862698af
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-27 19:52:11 +0530

    implement validation middleware for GET /abc endpoint

--- Express/src/router/test.mjs
+++ Express/src/router/test.mjs
@@ -1,18 +1,96 @@
 import {Router} from 'express'
-import testMiddleware from '../utils/testMiddleware.mjs'
+import { matchedData, query, validationResult } from 'express-validator'
+import { validatorMethod } from '../utils/validatorMiddleware.mjs';
 
 const testRouter = Router()
 
-testRouter.get('/',
-    testMiddleware,
-    (req, res, next) => {
-        console.log('2')
-        next()
-    },
+testRouter.get('/abc',
+    validatorMethod(),
     (req, res) => {
-        console.log('3')
-        res.sendStatus(200)
-    }
-)
+
+    const r = validationResult(req)
+    const d = matchedData(req)
+    console.log(d)
+
+    if(r.array().length) return res.sendStatus(400);
+
+    res.sendStatus(200)
+})
 
 export default testRouter
+
+
+/*
+
+testRouter.get('/abc',
+    query("name").isString().withMessage('not a String'),
+    query("age").isNumeric().withMessage('not a number'),
+    (req, res) => {
+
+    const r = validationResult(req)
+    console.log(r.array())
+
+    if(r.array().length) return res.sendStatus(400);
+
+    res.sendStatus(200)
+})
+
+*/
+
+
+/*
+
+testRouter.get('/abc/:name/:age',
+    param("name").isString().withMessage('not a String'),
+    param("age").isNumeric().withMessage('not a number'),
+    (req, res) => {
+
+    const r = validationResult(req)
+    console.log(r.array())
+
+    if(r.array().length) return res.sendStatus(400);
+
+    res.sendStatus(200)
+})
+
+*/
+
+/*
+
+testRouter.get('/abc',
+    body("name").isString().withMessage('not a String'),
+    body("age").isNumeric().withMessage('not a number'),
+    (req, res) => {
+
+    const r = validationResult(req)
+    console.log(r.array())
+
+    if(r.array().length) return res.sendStatus(400);
+
+    res.sendStatus(200)
+})
+
+*/
+
+/*
+testRouter.get('/abc',
+    checkSchema({
+    name:{
+        isEmail: true,
+        errorMessage: 'not a email'
+    },
+    age:{
+        isNumeric: true,
+        errorMessage: 'not a number'
+    }
+    }),
+    (req, res) => {
+
+    const r = validationResult(req)
+    console.log(r.array())
+
+    if(r.array().length) return res.sendStatus(400);
+
+    res.sendStatus(200)
+})
+*/
