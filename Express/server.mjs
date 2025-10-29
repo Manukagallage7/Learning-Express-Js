520fcae61949c573b3cc13eb392fbe166f96b818
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-29 23:20:04 +0530

    adding profileRouter

--- Express/server.mjs
+++ Express/server.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import productRouter from './src/router/product.mjs';
 import userRouter from './src/router/user.mjs';
 import testRouter from './src/router/test.mjs';
+import profileRouter from './src/router/profile.mjs';
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(express.json());
 server.use('/api/v1/user', userRouter);
 server.use('/api/v1/product', productRouter);
 server.use('/api/v1/test', testRouter);
+server.use('/api/v1/profile', profileRouter);
 
 server.listen(5000, ()=> {
     console.log('Server is running on http://localhost:5000');
