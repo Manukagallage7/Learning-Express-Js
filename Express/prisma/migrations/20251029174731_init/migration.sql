3caf806d3d6e9160e3fa7c5a90a973365a4991d9
Manukagallage7 <manukakavinda1110@gmail.com>
2025-10-29 23:20:23 +0530

    adding product and profile model

---
+++ Express/prisma/migrations/20251029174731_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "Id" SERIAL NOT NULL,
+    "Image" TEXT NOT NULL,
+    "UserId" INTEGER NOT NULL,
+    "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "UpdatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateTable
+CREATE TABLE "product" (
+    "Id" SERIAL NOT NULL,
+    "Name" TEXT NOT NULL,
+    "UserId" INTEGER NOT NULL,
+    "Price" DOUBLE PRECISION NOT NULL,
+    "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "UpdatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_UserId_key" ON "Profile"("UserId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "product" ADD CONSTRAINT "product_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
