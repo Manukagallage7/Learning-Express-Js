a08b668e22f0434ef4018db253746b2be61a3d56
Manukagallage7 <manukakavinda1110@gmail.com>
2025-11-01 11:24:53 +0530

    Adding checkAuth in Profile Router

--- Express/server.mjs
+++ Express/server.mjs
@@ -3,6 +3,7 @@ import productRouter from './src/router/product.mjs';
 import userRouter from './src/router/user.mjs';
 import testRouter from './src/router/test.mjs';
 import profileRouter from './src/router/profile.mjs';
+import { checkAuth } from './src/utils/authMiddleware.mjs';
 
 const server = express();
 
@@ -10,7 +11,7 @@ server.use(express.json());
 server.use('/api/v1/user', userRouter);
 server.use('/api/v1/product', productRouter);
 server.use('/api/v1/test', testRouter);
-server.use('/api/v1/profile', profileRouter);
+server.use('/api/v1/profile',checkAuth, profileRouter);
 
 server.listen(5000, ()=> {
     console.log('Server is running on http://localhost:5000');
