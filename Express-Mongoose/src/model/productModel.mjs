399e10f99454b684fbf772dfef5dbc69cab57dc5
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-08 08:30:49 +0530

    Add Product model with schema for MongoDB using Mongoose

---
+++ Express-Mongoose/src/model/productModel.mjs
@@ -0,0 +1,18 @@
+import {model, Schema, Types} from 'mongoose';
+
+const productSchema = new Schema({
+    title: String,
+    image: String,
+    user: {
+        type: Types.objectId,
+        ref: 'User',
+        required: true
+    }
+},
+{
+    timestamps: true
+});
+
+const Product = model('Product', productSchema);
+
+export default Product;
