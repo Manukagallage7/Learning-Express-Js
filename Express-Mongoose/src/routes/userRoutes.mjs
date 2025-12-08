c7a9ab4b0bfebf623a675e81888490ff8d164f2d
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-08 10:24:37 +0530

    Add product routes to user routes for handling product-related requests

--- Express-Mongoose/src/routes/userRoutes.mjs
+++ Express-Mongoose/src/routes/userRoutes.mjs
@@ -1,9 +1,10 @@
 import {Router} from 'express';
 import userRouter from '../controller/userController.mjs';
+import productRouter from '../controller/productController.mjs';
 
 const rootRouter = Router()
 
 rootRouter.use("/user", userRouter);
-
+rootRouter.use("/product", productRouter);
 
 export default rootRouter;
