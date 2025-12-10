cb38ddbbec1bb83a10030ad6ce575b65ceea7627
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-10 12:26:24 +0530

    Add category routes to user routes for handling category-related requests

--- Express-Mongoose/src/routes/userRoutes.mjs
+++ Express-Mongoose/src/routes/userRoutes.mjs
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import userRouter from '../controller/userController.mjs';
 import productRouter from '../controller/productController.mjs';
+import categoryRouter from '../controller/categoryController.mjs';
 
 const rootRouter = Router()
 
 rootRouter.use("/user", userRouter);
 rootRouter.use("/product", productRouter);
+rootRouter.use("/category", categoryRouter);
 
 export default rootRouter;
