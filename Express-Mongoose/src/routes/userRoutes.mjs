b5ba4f60c50fced1522e4c782e26bed1d8a083b9
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-11 10:33:55 +0530

    Add seeds route to integrate seedsController into the main routing

--- Express-Mongoose/src/routes/userRoutes.mjs
+++ Express-Mongoose/src/routes/userRoutes.mjs
@@ -2,11 +2,13 @@ import {Router} from 'express';
 import userRouter from '../controller/userController.mjs';
 import productRouter from '../controller/productController.mjs';
 import categoryRouter from '../controller/categoryController.mjs';
+import seedsRouter from '../controller/seedsController.mjs';
 
 const rootRouter = Router()
 
 rootRouter.use("/user", userRouter);
 rootRouter.use("/product", productRouter);
 rootRouter.use("/category", categoryRouter);
+rootRouter.use("/seeds", seedsRouter);
 
 export default rootRouter;
