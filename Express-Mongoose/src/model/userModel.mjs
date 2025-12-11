84ab4763d843763b661b2d8b345785b6ffc8a6e6
Manukagallage7 <manukakavinda1110@gmail.com>
2025-12-11 11:40:12 +0530

    Remove uniqueness constraint from profile reference in User schema

--- Express-Mongoose/src/model/userModel.mjs
+++ Express-Mongoose/src/model/userModel.mjs
@@ -32,8 +32,7 @@ const UserSchema = new Schema({
     },
     profile: {
         type: Schema.Types.ObjectId,
-        ref: "Profile",
-        unique: true
+        ref: "Profile"
     },
     products:[
         {
